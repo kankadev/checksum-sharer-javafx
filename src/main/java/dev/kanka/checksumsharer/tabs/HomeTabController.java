9a1ff65bef0b99000d0a0250365fd5b43a1a8f20
ASUS <kanka.dev22@gmail.com>
2022-01-10 22:18:55 +0300

    filterable TableView

--- src/main/java/dev/kanka/checksumsharer/tabs/HomeTabController.java
+++ src/main/java/dev/kanka/checksumsharer/tabs/HomeTabController.java
@@ -3,12 +3,13 @@ package dev.kanka.checksumsharer.tabs;
 import dev.kanka.checksumsharer.ChecksumSharerApplication;
 import dev.kanka.checksumsharer.dao.FileDAO;
 import dev.kanka.checksumsharer.models.File;
+import dev.kanka.checksumsharer.utils.FileUtil;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
 import org.apache.logging.log4j.LogManager;
@@ -22,10 +23,17 @@ import java.util.ResourceBundle;
 import static dev.kanka.checksumsharer.dao.FileDAO.insertFilesIntoDB;
 
 public class HomeTabController implements Initializable {
+
     private static final Logger logger = LogManager.getLogger();
 
     @FXML
+    TextField searchTextField;
+
+    @FXML
     TableView<File> historyTableView;
+
+    @FXML
+    TableColumn<File, Integer> idColumn;
 
     @FXML
     TableColumn<File, Timestamp> dateColumn;
@@ -40,10 +48,7 @@ public class HomeTabController implements Initializable {
     TableColumn<File, Long> lastModifiedColumn;
 
     @FXML
-    TableColumn<File, Long> fileSizeColumn;
-
-    @FXML
-    TableColumn<File, Integer> idColumn;
+    TableColumn<File, Number> fileSizeColumn;
 
     @FXML
     Button fileChooserButton;
@@ -59,14 +64,66 @@ public class HomeTabController implements Initializable {
     private void initHistoryTableView() {
         historyTableView.setPlaceholder(new Label("No files for which the checksums have been calculated yet."));
 
-        historyTableView.setItems(FileDAO.getFiles());
-
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        idColumn.setMinWidth(40);
+        idColumn.setMaxWidth(50);
         dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
         fileNameColumn.setCellValueFactory(new PropertyValueFactory<>("fileName"));
         fullPathColumn.setCellValueFactory(new PropertyValueFactory<>("fullPath"));
         lastModifiedColumn.setCellValueFactory(new PropertyValueFactory<>("lastModified"));
+
         fileSizeColumn.setCellValueFactory(new PropertyValueFactory<>("fileSize"));
-        idColumn.setCellValueFactory(new PropertyValueFactory<>("id"));
+        fileSizeColumn.setCellFactory(tc -> new TableCell<>() {
+            @Override
+            protected void updateItem(Number value, boolean empty) {
+                super.updateItem(value, empty);
+                if (value == null || empty) {
+                    setText("");
+                } else {
+                    setText(FileUtil.humanReadableByteCountBin((Long) value) + " (" + value + " Bytes)");
+                }
+            }
+        });
+
+        ObservableList<File> filesList = FileDAO.getFiles();
+        FilteredList<File> filteredList = new FilteredList<>(filesList, b -> true);
+
+        searchTextField.textProperty().addListener((observable, oldValue, newValue) -> filteredList.setPredicate(file -> {
+
+            // if filter is empty, display all files.
+            if (newValue == null || newValue.isEmpty()) {
+                return true;
+            }
+
+            String searchString = newValue.toLowerCase();
+
+            if (String.valueOf(file.getId()).contains(searchString)) {
+                return true;
+            }
+
+            if (file.getFileName().contains(searchString)) {
+                return true;
+            }
+
+            if (file.getFullPath().contains(searchString)) {
+                return true;
+            }
+
+            if (String.valueOf(file.getFileSize()).contains(searchString)) {
+                return true;
+            }
+
+            if (String.valueOf(file.getLastModified()).contains(searchString)) {
+                return true;
+            }
+
+            return false;
+        }));
+
+        SortedList<File> sortedList = new SortedList<>(filteredList);
+        sortedList.comparatorProperty().bind(historyTableView.comparatorProperty());
+
+        historyTableView.setItems(sortedList);
     }
 
     private void registerEventHandler() {
@@ -82,4 +139,5 @@ public class HomeTabController implements Initializable {
         insertFilesIntoDB(files);
         return files;
     }
+
 }
