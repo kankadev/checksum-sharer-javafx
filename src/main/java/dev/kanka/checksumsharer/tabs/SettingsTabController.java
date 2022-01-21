10a6e2a87f7513c7b6493df70266aa9812a62110
Akay <mail@kanka.dev>
2022-01-22 02:42:57 +0300

    implementation of checksum calculation (not stable at the moment)

--- src/main/java/dev/kanka/checksumsharer/tabs/SettingsTabController.java
+++ src/main/java/dev/kanka/checksumsharer/tabs/SettingsTabController.java
@@ -4,10 +4,12 @@ import com.dlsc.formsfx.model.structure.Field;
 import com.dlsc.formsfx.model.structure.Form;
 import com.dlsc.formsfx.model.structure.Group;
 import com.dlsc.formsfx.view.controls.SimpleRadioButtonControl;
+import com.dlsc.formsfx.view.controls.SimpleTextControl;
 import com.dlsc.formsfx.view.renderer.FormRenderer;
 import dev.kanka.checksumsharer.models.Settings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
 
 import java.net.URL;
@@ -15,25 +17,49 @@ import java.util.ResourceBundle;
 
 public class SettingsTabController implements Initializable {
 
-
     @FXML
     VBox vBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
-
         Settings settings = new Settings();
 
-        Form dateFormatForm = Form.of(
-                Group.of(Field.ofSingleSelectionType(settings.getDateFormatProperty(), 0)
-                        .label("Date Format")
-                        .labelDescription("Select how the date format should be displayed.")
-                        .render(new SimpleRadioButtonControl<>())
+        Form form = Form.of(
+                Group.of(Field.ofSingleSelectionType(settings.languageProperty(), 0)
+                                .label("Language")
+                                .labelDescription("Select your language.")
+                                .render(new SimpleRadioButtonControl<>()),
+                        Field.ofSingleSelectionType(settings.dateFormatProperty(), 0)
+                                .label("Date Format")
+                                .labelDescription("Select how the date format should be displayed.")
+                                .render(new SimpleRadioButtonControl<>()),
+                        Field.ofStringType(settings.exportLocationProperty())
+                                .label("Export Location")
+                                .labelDescription("Select where to save the checksum files.")
+                                .render(new SimpleTextControl())
                 )
-        ).title("Date Format");
+        ).title("Settings");
 
-        vBox.getChildren().add(new FormRenderer(dateFormatForm));
+        Button saveButton = createSaveButton(form);
 
+        vBox.getChildren().addAll(new FormRenderer(form), saveButton);
+
+    }
+
+    private Button createSaveButton(Form form) {
+        Button btn = new Button("Save");
+
+        btn.getStyleClass().addAll("btn", "btn-primary");
+
+        btn.disableProperty().bind(form.changedProperty().not().and(form.validProperty()));
+
+        btn.setOnAction(event -> {
+            if (form.isValid()) {
+                form.persist();
+            }
+        });
+
+        return btn;
     }
 }
